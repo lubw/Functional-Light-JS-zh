52c54d2a54f00305f1570a72ab4d81988445b85c
gdut-yy <gdut_yy@163.com>
2019-10-06 12:03:07 +0800

    轻量函数式 JavaScript 中文

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # Functional-Light-JS-zh
 轻量函数式 JavaScript 中文
+
+<img src="./docs/images/marketing/front-cover-small.png" width=40%>
+
+## 本地开发 & 阅读
+
+本项目基于 vuepress 进行开发，以提供比 github mardown 更佳的阅读体验
+依赖于 `node.js`、`yarn`、`vuepress` 等环境
+
+```sh
+# node
+node -v
+> v10.14.1
+# yarn
+yarn -v
+> 1.13.0
+# vuepress
+yarn global add vuepress
+
+# 本地开发
+git clone https://github.com/gdut-yy/CSS-The-Definitive-Guide-4th-zh.git
+cd CSS-The-Definitive-Guide-4th-zh
+yarn docs:dev
+
+# 本地阅读
+http://localhost:8080/
+```
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,31 @@
+// .vuepress/config.js
+module.exports = {
+  themeConfig: {
+    // 最大深度
+    sidebarDepth: 4,
+    // 导航栏
+    nav: [],
+    // 侧边栏
+    sidebar: {
+      '/': [
+        '',
+        'foreword.md',
+        'preface.md',
+        'ch1.md',
+        'ch2.md',
+        'ch3.md',
+        'ch4.md',
+        'ch5.md',
+        'ch6.md',
+        'ch7.md',
+        'ch8.md',
+        'ch9.md',
+        'ch10.md',
+        'ch11.md',
+        'apA.md',
+        'apB.md',
+        'apC.md',
+      ]
+    }
+  }
+}
